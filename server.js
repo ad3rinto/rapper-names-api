c539f07fbcba52253663102f69dba2c8e1e957df
aaderinto <aad3rinto@gmail.com>
2023-09-09 19:42:43 +0100

    add single api route

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express")
 const PORT = 8000;
-
+const rapperList = {
+    "birthname": "John Douglas",
+    "age": 45,
+    "birthLocation": "Inglewood, California"
+}
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/api", (req, res) => {
+    res.json(rapperList)
 })
 
 app.listen(PORT, () => {
