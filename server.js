59e5a3b216ad8d2d7ff4c8e6ec630d597bb732fe
aaderinto <aad3rinto@gmail.com>
2023-09-10 16:28:50 +0100

    added more details to rapper names object

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
 const express = require("express")
 const PORT = 8000;
 const rapperList = {
-    "birthname": "John Douglas",
-    "age": 45,
-    "birthLocation": "Inglewood, California"
+    "50 cents": {
+        "birthName": "Curtis James Jackson",
+        "biggest hit": "In da club"
+    },
+    "cardi B": {
+        "birthName": "Belcalis Almanaz",
+        "biggest hit": "Bodak Yellow"
+    },
+    "chance the rapper": {
+        "birthName": "Chancelor Jonathan Bennet",
+        "biggest hit": "No problem"
+    },
+    "dr. dre": {
+        "birthName": "Andre Romelle Young",
+        "biggest hit": "No Diggity"
+    },
+    "drake": {
+        "birthName": "Aubre Drake Graham",
+        "biggest hit": "God's plan"
+    },
+    "grandmaster Flash": {
+        "birthName": "Joseph Sadller",
+        "biggest hit": "The Message"
+    },
+    "ice cube": {
+        "birthName": "O'shea Jackson",
+        "biggest hit": "It was a good day"
+    },
+    "queen latifah": {
+        "birthName": "Dana Elaine Owens",
+        "biggest hit": "U.N.I.T.Y"
+    },
+    "snoop dog": {
+        "birthName": "Calvin Cordozar Broadus Jnr.",
+        "biggest hit": "Drop it lit its hot"
+    },
+    "the notorious b.i.g": {
+        "birthName": "Christopher George Laroe Wallace",
+        "biggest hit": "Mo money mo problems"
+    },
+    "tupac": {
+        "birthName": "Tupac Amaro Shakur",
+        "biggest hit": "Dear Mama"
+    }
+
+
 }
 
 const app = express();
@@ -16,6 +59,15 @@ app.get("/api", (req, res) => {
     res.json(rapperList)
 })
 
+app.get("/api" /: rapperName, (req, res) => {
+    const realName = req.body.rapperName.toLowerCase();
+    if (realName in rapperList) {
+        res.json(rapperList[realName])
+    }
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} for now.`)
 })
+
